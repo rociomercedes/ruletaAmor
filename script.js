a8ad3b93c01681755f3bc1f54fafcc81353b1191
rociomercedes <76015992+rociomercedes@users.noreply.github.com>
2025-02-12 11:27:04 -0300

    Modificacion js

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ let mensajes = [
 function girarRuleta() {
     let ruleta = document.getElementById("ruleta");
     let mensaje = document.getElementById("mensaje");
-    let giro = Math.floor(3600 + Math.random() * 360); // Rota entre 3600 y 3960 grados
+    let giro = Math.floor(3600 + Math.random() * 360); 
     ruleta.style.transform = `rotate(${giro}deg)`;
     
     setTimeout(() => {
         let anguloFinal = giro % 360;
         let index = Math.floor(anguloFinal / 60);
         mensaje.innerText = mensajes[index];
-    }, 4000); // Espera la animación antes de mostrar el mensaje
+    }, 4000); 
 }
